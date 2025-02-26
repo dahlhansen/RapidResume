097dd1a1523becb89521e20f89069b355d2102f9
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2025-02-26 09:01:41 -0500

    feat: Added Resume class needed for transfering over object.

---
+++ RapidResume/RapidResume/RapidResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapidResumeApp.swift
+//  RapidResume
+//
+//  Created by Frederik Dahl Hansen on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RapidResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RapidResume/RapidResume/Resume.swift
@@ -0,0 +1,8 @@
+//
+//  Resume.swift
+//  RapidResume
+//
+//  Created by Frederik Dahl Hansen on 26/02/2025.
+//
+
+import Foundation
